987af48e83deae32bcdc479be2965e1c24b7e0ac
mayboolean <>
2024-11-22 14:52:37 -0500

    finished activity

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 // Add javascript code to Party Parrot Clicker such that:
 
+const state = {
+  count: 0,
+
+};
 // 1. A click on the Party Parrot increases the counter by 1
+const addToCounter = () => {
+  const currentCount = document.getElementById('click-count');
+  state.count += 1;
+  currentCount.textContent = state.count;
+  if (state.count > 5) {
+    const parrotImg = document.querySelector('img');
+    parrotImg.classList.add('enlarge');
+  }
+};
+
+const registerEventHandlers = () => {
+  const addCount = document.querySelector('#add-parrot');
+  addCount.addEventListener('click', addToCounter);
+  const resetCount = document.querySelector('#reset');
+  resetCount.addEventListener('click', resetCounter);
+};
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
+
+
 // 2. Add a button called ‘reset’ that will reset the
 //    click count to ‘0’.
+
+const resetCounter = () => {
+  const currentCount = document.getElementById('click-count');
+  state.count = 0;
+  currentCount.textContent = state.count;
+};
+
 // 3. Add an effect whenever the clickCount
 //    reaches a specific number.
 //    (Ex: make parrot bigger, smaller, change the text/emoji,
 //    etc.)
+
+
 
 // Extra:
 // // Box 1: When the mouse hovers over Box 1, switch the background to pink
